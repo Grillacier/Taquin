55175ef4c3268ae0d5c847ce2196b8ed5042b298
SIMONEAU louis-alexei <louis.simoneau.pro@gmail.com>
2021-02-05 14:35:10 +0100

    Creation de la class Case

---
+++ Case.java
@@ -0,0 +1,12 @@
+
+public class Case {
+    private int numero; // numero d'une case (de 1 à 15 dans un jeu 4x4)
+
+    public Case(int n){
+        this.numero = n;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+}
