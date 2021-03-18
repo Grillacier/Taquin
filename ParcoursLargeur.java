19c4b2f6a8677913ee5522ecb139a7b5aafbc51d
BA ibrahima <ibrahima.bamoctar@gmail.com>
2021-03-18 22:45:06 +0100

    Upload New File

---
+++ ParcoursLargeur.java
@@ -0,0 +1,11 @@
+public class ParcoursLargeur{
+    private Plateau p;
+    private Queue q;
+
+    public ParcoursLargeur(){
+        this.p=null;
+        this.q=null;
+        //this.p=fct envoyant un taquin soluble mélangé
+        //this.q=new Queue(p);
+    }
+}
